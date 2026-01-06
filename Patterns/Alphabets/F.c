b7b892b67c2799e018716c79edc37224b4029c45
Priya Patil <priyapatil8277@gmail.com>
2026-01-06 21:54:48 +0530

    Add files via upload

---
+++ Patterns/Alphabets/F.c
@@ -0,0 +1,30 @@
+//* * * *
+//*
+//*
+//* * * *
+//*
+//*
+//*
+
+#include<stdio.h>
+
+void main()
+{
+	int i,j;
+	for(i=1;i<=7;i++)
+	{
+		for(j=1;j<=4;j++)
+		{
+			if(j==1 || i==1 || i==4)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+}
+
