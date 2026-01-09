55b00845d668adf0cb8d5b4a6b929fe6f22c3a8f
Priya Patil <priyapatil8277@gmail.com>
2026-01-09 18:39:42 +0530

    Add files via upload

---
+++ PR=04/Q5.c
@@ -0,0 +1,28 @@
+//        5
+//      4 5 4
+//    3 4 5 4 3
+//  2 3 4 5 4 3 2
+//1 2 3 4 5 4 3 2 1
+
+#include<stdio.h>
+
+void main()
+{
+	int i,j,k;
+	for(i=5;i>=1;i--)
+	{
+		for(k=i;k>1;k--)
+		{
+			printf("  ");
+		}
+		for(j=i;j<=5;j++)
+		{
+			printf("%d ",j);
+		}
+		for(j=4;j>=i;j--)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+}
