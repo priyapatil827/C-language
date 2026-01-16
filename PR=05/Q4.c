60bbdbc2004a1f6d4cfb20d37a4d3cb34044c874
Priya Patil <priyapatil8277@gmail.com>
2026-01-16 13:27:32 +0530

    Update Q4.c

--- PR=05/Q4.c
+++ PR=05/Q4.c
@@ -1,46 +1,38 @@
-#include <stdio.h>
 
 int main()
 {
-   int r, c, i, j;
-
-   printf("Enter rows and columns: ");
-   scanf("%d %d", &r, &c);
-
-   int a[r][c];
-
-   printf("\nEnter elements of matrix:\n");
-   for(i = 0; i < r; i++)
-   {
-      for(j = 0; j < c; j++)
-      {
-         printf("a[%d][%d] = ", i, j);
-         scanf("%d", &a[i][j]);
-      }
-   }
-
-   printf("\nSum of each row:\n");
-   for(i = 0; i < r; i++)
-   {
-      int rowSum = 0;
-      for(j = 0; j < c; j++)
-      {
-         rowSum += a[i][j];
-      }
-      printf("Row %d = %d\n", i + 1, rowSum);
-   }
-   
-   printf("\nSum of each column:\n");
-   for(j = 0; j < c; j++)
-   {
-      int colSum = 0;
-      for(i = 0; i < r; i++)
-      {
-         colSum += a[i][j];
-      }
-      printf("Column %d = %d\n", j + 1, colSum);
-   }
-
-   return 0;
+	int r,c,i,j,sum=0;
+	printf("enter a row:");
+	scanf("%d",&r);
+	printf("enter a col:");
+	scanf("%d",&c);
+	int a[r][c];
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("enter a[%d][%d]",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d ",a[i][j]);
+			
+		}
+	}
+	printf("\n");
+		for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			sum=sum+a[i][j];
+			
+		}
+	
+	}
+	printf("sum is %d",sum);
 }
 
